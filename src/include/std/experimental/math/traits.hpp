87c9f5fface50d5269ee7fcbc4632cd3a4d1285a
Jayesh Badwaik <public@jayeshbadwaik.in>
2019-06-01 12:03:04 +0200

    + traits : add tests for is_numeric_field

--- src/include/std/experimental/math/traits.hpp
+++ src/include/std/experimental/math/traits.hpp
@@ -1,6 +1,7 @@
 #ifndef STD_EXPERIMENTAL_MATH_TRAITS_HPP
 #define STD_EXPERIMENTAL_MATH_TRAITS_HPP
 
+#include <std/experimental/math/detail/traits.hpp>
 #include <type_traits>
 
 namespace std::experimental::math {
@@ -15,6 +16,9 @@ struct is_numeric_field<double> : std::true_type {
 template <>
 struct is_numeric_field<float> : std::true_type {
 };
+
+template <class T>
+inline constexpr bool is_numeric_field_v = is_numeric_field<T>::value;
 } // namespace std::experimental::math
 
 #endif // STD_EXPERIMENTAL_MATH_TRAITS_HPP
--- test/lib/unit/traits.t.cpp
+++ test/lib/unit/traits.t.cpp
@@ -1,1 +1,12 @@
+#define CATCH_CONFIG_FAST_COMPILE
+#include <catch2/catch.hpp>
 #include <std/experimental/math/traits.hpp>
+
+namespace math = std::experimental::math;
+
+TEST_CASE("traits: test01: is_numeric_field", "[traits]")
+{
+  static_assert(math::is_numeric_field_v<double>);
+  static_assert(math::is_numeric_field_v<float>);
+  static_assert(not math::is_numeric_field_v<int>);
+}
